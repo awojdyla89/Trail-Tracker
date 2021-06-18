e61e0038187d0ba1d99730a00d56a3d95631ee79
Adam Wojdyla <awojdyla@gmail.com>
2021-06-18 11:32:07 -0600

    commented new routes

--- routes/trails.js
+++ routes/trails.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const trailsCtrl = require('../controllers/trails');
 
-// this is /trails/ server.js has /trails defined already
+// index.ejs in trails view - /trails/ 
 router.get('/', trailsCtrl.index);
+
+//this is /trails/new
+//view form to create new trail - /trails/new
 router.get('/new', trailsCtrl.new);
 
 
