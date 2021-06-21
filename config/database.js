8e77a699b4449ba922cc857fe6fec599e9c14da5
Adam Wojdyla <awojdyla@gmail.com>
2021-06-21 13:09:21 -0600

    connected project to mondoDB cluster

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // replace your database connection string here!!!!!!!
-mongoose.connect('mongodb://localhost/users' ,{
+mongoose.connect(process.env.DATABASE_URL,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
