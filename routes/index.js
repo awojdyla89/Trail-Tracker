9bcda3795c06fa4948f2fef4f62637030a9484eb
Adam Wojdyla <awojdyla@gmail.com>
2021-06-23 16:18:33 -0600

    changed success redirect and landing page redirect

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 router.get('/', function(req, res) {
   // Where do you want to go for the root route
   // in the student demo this was res.redirect('/students'), what do you want?
-  res.redirect('/trails')
+  res.redirect('/users')
   //res.redirect('/trails');
 });
 
@@ -19,7 +19,7 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // where do you want the client to go after you login 
+    successRedirect : '/trails', // where do you want the client to go after you login 
     failureRedirect : '/' // where do you want the client to go if login fails
   }
 ));
